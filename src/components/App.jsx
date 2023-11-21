c447dffac74c4f45d6a3d978deae17c455b78a86
bpb2008 <brittanythompson08@gmail.com>
2023-11-21 11:29:45 -0600

    finish API call

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./styles/App.css";
 import PokeList from "./PokeList";
 import DetailView from "./DetailView";
+import Pokemon from "./Pokemon";
 
 function App() {
-  const [pokemon, setPokemon] = useState({});
+  const [pokemon, setPokemon] = useState({
+    sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png`,
+    type: "grass",
+    id: 1,
+    name: "Bulbasaur",
+  });
 
-  const handleOnClick = (id) => {
-    .fetch(`http;//pokeapi.co/api/v2/pokemon/${id}/`)
-      .then(response => response.json()) 
-      .then(data => {
-        const pokemon = new Pokemon(data);
-        console.log(pokemon); 
-      })
-      .catch(err => console.log(err)); 
-    console.log(id);
+  const handleOnClick = async (id) => {
+    try {
+      const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`);
+      const data = await response.json();
+      const pokemonData = new Pokemon(data);
+      setPokemon(pokemonData);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="App">
       <PokeList handleOnClick={handleOnClick} />
-      <DetailView pokemon={setPokemon} />
+      <DetailView pokemon={pokemon} />
     </div>
   );
 }
--- src/components/PokeCell.jsx
+++ src/components/PokeCell.jsx
@@ -10,7 +10,7 @@ const PokeCell = ({ pokeClass, handleOnClick }) => {
       onClick={() => handleOnClick(id)}
       className="poke-cell"
       style={style}
-    ></button>
+    />
   );
 };
 
